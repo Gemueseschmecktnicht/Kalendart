fc37c0a2857d40757378f95190de24315be8ea5e
Gemueseschmecktnicht <camillox3@web.de>
2018-05-23 14:43:26 +0200

    + Fehlerfenster, bei Nichteingabe von Name, Datum, Uhrzeit

--- app/src/main/java/de/kuribo/kalendart/AddEventActivity.java
+++ app/src/main/java/de/kuribo/kalendart/AddEventActivity.java
@@ -1,18 +1,24 @@
 package de.kuribo.kalendart;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
+
+import static android.app.PendingIntent.getActivity;
 
 public class AddEventActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener{
 
@@ -44,18 +50,36 @@ public class AddEventActivity extends AppCompatActivity implements TimePickerDia
         setContentView(R.layout.addevent_layout); //Layout verknüpfen
         btnFinalAddEvent = (Button) findViewById(R.id.btnFinalAddEvent);
         btnDiscard = (Button) findViewById(R.id.btnDiscard);
-        Button txtUhrzeit = (Button) findViewById(R.id.txtZeit);
-        Button txtDatum = (Button) findViewById(R.id.txtDatum);
+        final Button txtUhrzeit = (Button) findViewById(R.id.txtZeit);
+        final TextView txtUhrzeitShow = (TextView) findViewById(R.id.txtUhrzeitShow);
+        final Button txtDatum = (Button) findViewById(R.id.txtDatum);
+        final TextView txtDatumShow = (TextView) findViewById(R.id.txtDatumShow);
         final TextView txtName = (TextView) findViewById(R.id.txtName);
+        final EditText etxtName = (EditText) findViewById(R.id.txtName);
+
 
         //Ereignis hinzufügen geklickt
         btnFinalAddEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                name = txtName.getText().toString();
-                Toast msg = Toast.makeText(getBaseContext(), "Variablen Gespeichert", Toast.LENGTH_LONG);
-                msg.show();
-                AddEventActivity.super.onBackPressed();
+                name = etxtName.getText().toString();
+
+                if (name.length() == 0) {
+                    IsNull("Namens");
+                } else {
+                    if(txtDatumShow.length() == 5){
+                        IsNull("Datums");
+                    } else {
+                        if (txtUhrzeitShow.length() == 7) {
+                            IsNull("Uhrzeit");
+                        } else {
+
+                            Toast msg = Toast.makeText(getBaseContext(), "Variablen Gespeichert", Toast.LENGTH_LONG);
+                            msg.show();
+                            AddEventActivity.super.onBackPressed();
+                        }
+                    }
+                }
             }
         });
 
@@ -120,6 +144,19 @@ public class AddEventActivity extends AppCompatActivity implements TimePickerDia
         txtDatumShow.setText(currentDateString);
     }
 
+    public void IsNull(String pIssue){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setCancelable(true);
+        alertDialogBuilder.setMessage("Das " +pIssue +"feld darf nicht leer sein");
+        alertDialogBuilder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
     //GETTER + SETTER
     public String getName(){
         return this.name;
